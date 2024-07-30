b2c0b4028efb09594484810481b4e6f085a9c11e
Jorge Rinaldi <jorge.rinaldi.1995@gmail.com>
2024-07-30 15:01:24 -0300

    Store product request

---
+++ app/Http/Requests/ProductResources/StoreProductRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests\ProductResources;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreProductRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|unique:products',
+            'price_per_unit' => 'required|numeric',
+            'basic_unit' => 'nullable|string',
+            'tax_percentage' => 'nullable|numeric',
+            'limited' => 'required|boolean',
+            'stock' => 'nullable|numeric',
+        ];
+    }
+}
