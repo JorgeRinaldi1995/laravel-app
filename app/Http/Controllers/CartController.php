24e7dc791eef3bea803a442b0bdb35fd21801165
Jorge Rinaldi <jorge.rinaldi.1995@gmail.com>
2024-07-30 17:06:15 -0300

    remove unused orm

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Cart;
 use App\Models\Product;
-use Illuminate\Support\Facades\DB;
 
 class CartController extends Controller
 {
